6dc980a67224cac02ba9800ccb20b7a293437e62
Isaque Hoffmeister <isaque.ha@gmail.com>
2025-10-20 20:09:57 -0400

    fix: use maps better

--- Todo.md
+++ Todo.md
@@ -1,17 +1,18 @@
 
 // D3 Viz
-// Simplify tooltips when hovering
-// Fix tooltips orientation
+
 // Real time data from APIs
   // Load only the first flight from each airport maybe
   // Planes go in the right velocity
-
-// Have Earth specular maps
 // Have 3D planes
-// D3
-// You could show Airports details, Airplane models and brands, Numbers of passengers, and all available and open data that will impress the recruiters to show I have D3 data visualization skills as well
 
-// From FLight Radar
+// Airplane models and brands, Numbers of passengers, and all available and open data that will impress the recruiters to show I have D3 data visualization skills as well
+
+<!-- https://openflights.org/# -->
+<!-- https://www.flightradar24.com/airport/yqb -->
+
+
+// From Flight Radar
 
 /* 
 CGOYU
--- src/components/Earth.tsx
+++ src/components/Earth.tsx
@@ -7,23 +7,49 @@ import FlightsLayer from "./FlightArcs";
 export default function Earth() {
   const mesh = useRef<THREE.Mesh>(null!);
 
-  const [colorMap, bumpMap] = useLoader(THREE.TextureLoader, [
+  // Load maps: color, normal, specular, clouds
+  const [colorMap, normalMap, specMap, cloudsMap] = useLoader(THREE.TextureLoader, [
     "/textures/8k_earth_daymap.jpg",
     "/textures/8k_earth_normal_map.jpg",
+    "/textures/8k_earth_specular_map.png",
+    "/textures/8k_earth_clouds.jpg",
   ]);
 
-  useFrame(() => {
-    // mesh.current.rotation.y += 0.0005;
-  });
-
   const earthRef = useRef<THREE.Mesh>(null!);
+  const cloudsRef = useRef<THREE.Mesh>(null!);
+
+  useFrame(() => {
+    // slow rotation for the earth
+    // if (mesh.current) mesh.current.rotation.y += 0.0005;
+    // slightly faster rotation for clouds for a parallax effect
+    if (cloudsRef.current) cloudsRef.current.rotation.y += 0.0004;
+  });
 
   return (
     <group ref={mesh}>
       {/* Earth mesh */}
       <mesh ref={earthRef}>
         <sphereGeometry args={[1, 64, 64]} />
-        <meshPhongMaterial map={colorMap} bumpMap={bumpMap} bumpScale={0.05} />
+        <meshPhongMaterial
+          map={colorMap}
+          normalMap={normalMap}
+          normalScale={new THREE.Vector2(1, 1)}
+          specularMap={specMap}
+          specular={new THREE.Color(0x222222)}
+          shininess={20}
+        />
+      </mesh>
+
+      {/* Clouds layer slightly above the surface */}
+      <mesh ref={cloudsRef}>
+        <sphereGeometry args={[1.01, 64, 64]} />
+        <meshPhongMaterial
+          map={cloudsMap}
+          transparent={true}
+          opacity={0.2}
+          depthWrite={false}
+          side={THREE.DoubleSide}
+        />
       </mesh>
 
       {/* Layers */}

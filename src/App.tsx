80626b585cfee7cc5f247243e034df18b7e1ca9b
Isaque Hoffmeister <isaque.ha@gmail.com>
2025-10-20 20:22:08 -0400

    fix: details

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import Globe from "./components/Globe";
-import Sidebar from "./components/SidePanel";
+import SidePanel from "./components/SidePanel";
 
 export default function App() {
   return (
@@ -8,7 +8,7 @@ export default function App() {
         <Globe />
       </div>
       <div className="absolute bottom-0 right-0 h-auto w-[30rem] border-l border-gray-800 bg-gray-900/80 p-4 overflow-y-auto">
-        <Sidebar />
+        <SidePanel />
       </div>
     </div>
   );
